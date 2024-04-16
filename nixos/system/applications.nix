3e7c7f74cb9877afce397f4e7df2fe6eabd27649
jonzuk <jonzukspam@gmail.com>
2024-04-16 12:13:42 -0400

    waybar fixed completely but no icons installed nerdfonts we will see what happens!

--- nixos/system/applications.nix
+++ nixos/system/applications.nix
@@ -3,6 +3,7 @@
 #List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+	pkgs.nerdfonts
 	xfce.thunar
 	xfce.thunar-volman
 	gvfs
