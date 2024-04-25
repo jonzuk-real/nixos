d20870d6b493331d7ed6001a27a1a683aa719d4d
jonzuk <jonzukspam@gmail.com>
2024-04-25 15:45:38 -0400

    updated firefox

--- nixos/system/applications.nix
+++ nixos/system/applications.nix
@@ -3,6 +3,7 @@
 #List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+	font-awesome
 	clematis
 	xorg.xhost
 	lxqt.lxqt-policykit
--- nixos/user/firefox.nix
+++ nixos/user/firefox.nix
@@ -26,19 +26,20 @@
         };
         in listToAttrs [
 	  (extension "unpaywall" "{f209234a-76f0-4735-9920-eb62507a54cd}")
-          (extension "tree-style-tab" "treestyletab@piro.sakura.ne.jp")
+#          (extension "tree-style-tab" "treestyletab@piro.sakura.ne.jp")
           (extension "adnauseam" "adnauseam@rednoise.org")
-          (extension "augmented-steam" "{1be309c5-3e4f-4b99-927d-bb500eb4fa88}")
-          (extension "tabliss" "extension@tabliss.io")
-          (extension "alike03s-subscription-info" "{7d7241f8-5541-4ab7-9c8a-ad15bd3aa4c7}")
-          (extension "libredirect" "7esoorv3@alefvanoon.anonaddy.me")
+#          (extension "augmented-steam" "{1be309c5-3e4f-4b99-927d-bb500eb4fa88}")
+#          (extension "tabliss" "extension@tabliss.io")
+#          (extension "alike03s-subscription-info" "{7d7241f8-5541-4ab7-9c8a-ad15bd3aa4c7}")
+#          (extension "libredirect" "7esoorv3@alefvanoon.anonaddy.me")
           (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
 	  (extension "darkreader" "addon@darkreader.org")
-          (extension "protondb-steam" "{30280527-c46c-4e03-bb16-2e3ed94fa57c}")
-          (extension "steam-url-opener" "{85252760-c586-4180-8788-c50234c0ac08}")
-          (extension "steam-database" "firefox-extension@steamdb.info")
+#          (extension "protondb-steam" "{30280527-c46c-4e03-bb16-2e3ed94fa57c}")
+#          (extension "steam-url-opener" "{85252760-c586-4180-8788-c50234c0ac08}")
+#          (extension "steam-database" "firefox-extension@steamdb.info")
 	  (extension "soundfixer" "soundfixer@unrelenting.technology")
 	  (extension "enhancer-for-youtube" "660d550a-2cff-40a8-b611-2e10b52fb49c")
+	  (extension "return-youtube-dislikes" "{762f9885-5a13-4abd-9c77-433dcd38b8fd}")
         ];
         # To add additional extensions, find it on addons.mozilla.org, find
         # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
