4a22d5faae56e24efbdca6774588ba1749b1b20f
jonzuk <jonzukspam@gmail.com>
2024-05-13 07:28:41 -0400

    home manager mod

--- nixos/system/applications.nix
+++ nixos/system/applications.nix
@@ -3,6 +3,7 @@
 #List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+	home-manager
 	openjdk
 	font-awesome
 	xorg.xhost
---
+++ nixos/user/bookmarks.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ...}:
+
+{
+	programs.firefox = {
+		enable = true;
+		profiles.default = {
+		
+	 bookmarks = [
+	 	{
+	 		name  = "wiki";
+	 		url = "https://wikipedia.org";
+	 	}
+	 ];
+	 
+	 };
+	};
+	}
