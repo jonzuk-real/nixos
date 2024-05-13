0882f8eb1c117d7c639cbd2e7b9effdae09f5f95
jonzuk <jonzukspam@gmail.com>
2024-05-13 14:31:17 -0400

    now im done

--- nixos/users.nix
+++ nixos/users.nix
@@ -5,11 +5,7 @@ users.users.jon = {
     isNormalUser = true;
     description = "jon";
     extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
-    packages = with pkgs; [
-
-
-    ];
-  };
+   };
 
   # Allow unfree packages
 nixpkgs.config = {  
