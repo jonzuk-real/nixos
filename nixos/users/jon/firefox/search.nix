13f3cac456baea67c7f376b10c5b66b6d87d1ad6
jonzuk <jonzukspam@gmail.com>
2024-05-15 17:29:04 -0400

    fixed stuff up

--- nixos/users/jon/firefox/search.nix
+++ nixos/users/jon/firefox/search.nix
@@ -13,26 +13,56 @@
                   ];
                 }];
                
-                definedAliases = [ "@np" ];
+                definedAliases = [ "@NixPackages" ];
               };
               "NixOS Wiki" = {
                 urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@nw" ];
+                definedAliases = [ "@NixWiki" ];
               };
               "Searx" = {
                 urls = [{ template = "https://searx.aicampground.com/?q={searchTerms}"; }];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@searx" ];
+                definedAliases = [ "@Searx" ];
+              };
+                "Yandex" = {
+                urls = [{ template = "https://yandex.com/search/?text={searchTerms}"; }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@Yandex" ];
+              };
+                "Same.Energy" = {
+                urls = [{ template = "https://same.energy/search?q={searchTerms}"; }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@Same.Energy" ];
               };
                 "StartPage" = {
                 urls = [{ template = "https://www.startpage.com/sp/search?query={searchTerms}"; }];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@sp" ];
+                definedAliases = [ "@Startpage" ];
               };
+                "archive.org" = {
+                urls = [{ template = "https://archive.org/search?query={searchTerms}"; }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@archive" ];
+              };
+                "DuckDuckGo" = {
+                urls = [{ template = "https://duckduckgo.com/?t=h_&q={searchTerms}"; }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@DuckDuckGo" ];
+              };
+#                "Brave Searcg" = {
+#                urls = [{ template = "https://search.brave.com/search?q={searchTerms}"; }];
+#                iconUpdateURL = "https://nixos.wiki/favicon.png";
+#                updateInterval = 24 * 60 * 60 * 1000; # every day
+#                definedAliases = [ "@Brave" ];
+#              };
               "Bing".metaData.hidden = true;
               "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
             };
