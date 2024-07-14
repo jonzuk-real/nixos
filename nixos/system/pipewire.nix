6602b68e316f62e44d4d9d958dee0b4f30c2fd6a
jonzuk <jonzukspam@gmail.com>
2024-07-14 13:58:19 -0400

    lower latency audio

--- nixos/system/pipewire.nix
+++ nixos/system/pipewire.nix
@@ -13,7 +13,7 @@
     jack.enable = true;
   };
 
-
+#Pipewire Low Latency
 services.pipewire.extraConfig.pipewire."92-low-latency" = {
   context.properties = {
     default.clock.rate = 48000;
@@ -22,4 +22,26 @@ services.pipewire.extraConfig.pipewire."92-low-latency" = {
     default.clock.max-quantum = 32;
   };
 };
+
+#PulseAudio Low Latency
+services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+  context.modules = [
+    {
+      name = "libpipewire-module-protocol-pulse";
+      args = {
+        pulse.min.req = "32/48000";
+        pulse.default.req = "32/48000";
+        pulse.max.req = "32/48000";
+        pulse.min.quantum = "32/48000";
+        pulse.max.quantum = "32/48000";
+      };
+    }
+  ];
+  stream.properties = {
+    node.latency = "32/48000";
+    resample.quality = 1;
+  };
+};
+
+
 }
