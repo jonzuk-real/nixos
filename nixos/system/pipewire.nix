88e887981a2d7c1462417f529da08793c8206de5
jonzuk <jonzukspam@gmail.com>
2024-07-14 14:00:32 -0400

    latency shit again

--- nixos/system/pipewire.nix
+++ nixos/system/pipewire.nix
@@ -43,5 +43,21 @@ services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
   };
 };
 
+#ALSA Low Latency
+services.pipewire.wireplumber.configPackages = [
+  (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
+    alsa_monitor.rules = {
+      {
+        matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+        apply_properties = {
+          ["audio.format"] = "S32LE",
+          ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+          ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+          -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
+        },
+      },
+    }
+  '')
+];
 
 }
