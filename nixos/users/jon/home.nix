ab927eef9910fb58c204ab226c3f254be2c840ce
jonzuk <jonzukspam@gmail.com>
2024-07-26 01:20:41 -0400

    added ladybird

--- nixos/users/jon/home.nix
+++ nixos/users/jon/home.nix
@@ -20,6 +20,7 @@ in
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   imports = [ 
+   ./ladybird.nix
   ./firefox/default.nix 
 #  ./system/kodi.nix
   ];
