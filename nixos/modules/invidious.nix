0b2a8febe7d7ce4b9bf70bc847baf07b47cb58b2
jonzuk <jonzukspam@gmail.com>
2024-07-28 10:47:09 -0400

    test

--- nixos/modules/invidious.nix
+++ nixos/modules/invidious.nix
@@ -1,11 +1,23 @@
 {
-
 services.invidious.enable = true;
-services.invidious.database.createLocally = true;
-services.postgresql.ensureDatabases = [
-  "jon"
-  "invidious"
-];
-
-
+  services.invidious.settings = lib.mkForce {
+    check_tables = true;
+    db = {
+      dbname = "invidious";
+      host = "";
+      password = "";
+      port = 5432;
+      user = "invidious";
+    };
+    host_binding = "0.0.0.0";
+    default_user_preferences = {
+      locale = "da";
+      region = "DK";
+    };
+    captions = [
+      "Danish"
+      "English"
+      "English (auto-generated)"
+    ];
+  };
 }
