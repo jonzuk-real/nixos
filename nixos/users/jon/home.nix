9193736d767bd071b8b78e18678b08c84a0d19ac
jonzuk <jonzukspam@gmail.com>
2024-07-28 11:33:00 -0400

    chrom

--- nixos/users/jon/home.nix
+++ nixos/users/jon/home.nix
@@ -103,7 +103,7 @@ in
         git # Distributed version control system
         syncthing # Open Source Continuous File Synchronization
         gdrive3 # Google Drive CLI Client
-#	chromium # Chromium Browser
+	chromium # Chromium Browser
         brave # Privacy-oriented browser for Desktop and Laptop computers
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
