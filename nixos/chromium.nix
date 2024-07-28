c9ae18060f7393fc313f2c55288ed2cfdf2a4d77
jonzuk <jonzukspam@gmail.com>
2024-07-28 11:19:31 -0400

    ungoogle

--- nixos/chromium.nix
+++ nixos/chromium.nix
@@ -1,6 +1,6 @@
 { pkgs, config, libs, ... }:
 { 
-environment.systemPackages = with pkgs; [ chromium ];
+environment.systemPackages = with pkgs; [ ungoogled-chromium ];
  programs.chromium = {
   enable = true;
   extensions = [
