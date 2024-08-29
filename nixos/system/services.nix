ae3855121f949d5ad2274328ba40b959d52e0eee
jonzuk <jonzukspam@gmail.com>
2024-08-28 23:25:19 -0400

    thunar?

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -2,7 +2,7 @@
  {
  # List services that you want to enable:
 	
-	programs.xfconf.enable = true
+	programs.xfconf.enable = true;
 	services.gvfs.enable = true; # Mount, trash, and other functionalities
 	services.tumbler.enable = true; # Thumbnail support for images
 	services.resolved.enable = true;
