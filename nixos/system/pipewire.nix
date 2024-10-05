c6005fb06a9186b7f109ff51e81dce7caca7ec19
jonzuk <jonzukspam@gmail.com>
2024-10-04 20:10:35 -0400

    test pulseaudio off

--- nixos/system/pipewire.nix
+++ nixos/system/pipewire.nix
@@ -8,35 +8,35 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+#    pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
   };
 
 #Pipewire Low Latency
-services.pipewire.extraConfig.pipewire."92-low-latency" = {
+services.pipewire.extraConfig.pipewire."92-high-quality" = {
   context.properties = {
-#    default.clock.rate = 48000;
-    default.clock.quantum = 32;
-    default.clock.min-quantum = 32;
-    default.clock.max-quantum = 32;
+    default.clock.rate = 192000;
+    default.clock.quantum = 1024;
+    default.clock.min-quantum = 1024;
+    default.clock.max-quantum = 8192;
   };
 };
 
 #PulseAudio Low Latency
-services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
-  context.modules = [
-    {
-      name = "libpipewire-module-protocol-pulse";
-      args = {
-        pulse.min.req = "256/44100";
+#services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+#  context.modules = [
+#    {
+#      name = "libpipewire-module-protocol-pulse";
+#      args = {
+#        pulse.min.req = "256/44100";
 #        pulse.default.req = "256/48000";
-        pulse.max.req = "1024/192000";
-        pulse.min.quantum = "256/44100";
-        pulse.max.quantum = "1024/192000";
-      };
-    }
-  ];
+#        pulse.max.req = "1024/192000";
+#        pulse.min.quantum = "256/44100";
+#        pulse.max.quantum = "1024/192000";
+#      };
+#    }
+#  ];
   stream.properties = {
     node.latency = "256/48000";
     resample.quality = 4;
@@ -45,15 +45,16 @@ services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
 
 #ALSA Low Latency
 services.pipewire.wireplumber.configPackages = [
-  (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
+  (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-highquality.lua" ''
     alsa_monitor.rules = {
       {
         matches = {{{ "node.name", "matches", "alsa_output.*" }}};
         apply_properties = {
-          ["audio.format"] = "S32LE",
-          ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
-          ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+          ["audio.format"] = "S24LE",
+          ["audio.rate"] = "192000", -- for USB soundcards it should be twice your desired rate
+          ["api.alsa.period-size"] = 1024, -- defaults to 1024, tweak by trial-and-error
           -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
+          ["api.alsa.headroom"] = 1024,
         },
       },
     }
