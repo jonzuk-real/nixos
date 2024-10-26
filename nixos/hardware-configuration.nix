d66166af345ba1312521628bd978677c6d53e717
jonzuk <jonzukspam@gmail.com>
2024-10-25 20:03:17 -0400

    test

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -38,7 +38,7 @@
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
 
-  boot.loader.systemd-boot.enable = true;
+#  boot.loader.systemd-boot.enable = true;
 #  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.loader.efi.efiSysMountPoint = "/efi";
