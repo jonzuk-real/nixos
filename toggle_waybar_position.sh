c91cd239b1895fcf8eea0ace3b2cc4ae613c6544
jonzuk <jonzukspam@gmail.com>
2024-12-14 19:55:43 -0500

    added KDE 6 again

--- toggle_waybar_position.sh
+++ toggle_waybar_position.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-current_position=$(grep '"position":' ~/.config/waybar/config | awk -F'"' '{print $4}')
+current_position=$(grep '"position":' /home/jon/.config/waybar/config | awk -F'"' '{print $4}')
 
 if [ "$current_position" = "top" ]; then
-    sed -i 's/"position": "top"/"position": "bottom"/' ~/.config/waybar/config
+    sed -i 's/"position": "top"/"position": "bottom"/' /home/jon/.config/waybar/config
 else
-    sed -i 's/"position": "bottom"/"position": "top"/' ~/.config/waybar/config
+    sed -i 's/"position": "bottom"/"position": "top"/' /home/jon/.config/waybar/config
 fi
 
 killall waybar
