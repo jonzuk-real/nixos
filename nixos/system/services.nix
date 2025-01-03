74e2724708a7e00c9682690d04021cb79daf5e30
jonzuk <jonzukspam@gmail.com>
2025-01-03 17:23:23 -0500

    gfds

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -61,12 +61,11 @@ services.desktopManager.plasma6.enable = true;
 #               xterm.enable = false;
 #               xfce.enable = true;
 #               lxqt.enable = true;
-      
 #               gnome.enable = true;
  };
         
 };
-services.desktopManager.plasma5.enable = true;
+#services.desktopManager.plasma5.enable = true;
 services.displayManager.sddm.enable = true;
 
 
