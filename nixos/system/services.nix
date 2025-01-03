bd7ee06e7186ac1814e41bf7d227316f83bb0b00
jonzuk <jonzukspam@gmail.com>
2025-01-03 17:21:14 -0500

    boutta buy a mac

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -61,12 +61,12 @@ services.desktopManager.plasma6.enable = true;
 #               xterm.enable = false;
 #               xfce.enable = true;
 #               lxqt.enable = true;
-               plasma6.enable = true;
+               #plasma6.enable = true;
 #               gnome.enable = true;
  };
         
 };
-
+services.plasma6.enable = true;
 services.displayManager.sddm.enable = true;
 
 
