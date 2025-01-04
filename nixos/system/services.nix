d7d9990fd944e40e530ae717b358f6b82eb0ef06
jonzuk <jonzukspam@gmail.com>
2025-01-03 22:18:41 -0500

    gdm

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -66,7 +66,7 @@ services.desktopManager.plasma6.enable = true;
         
 };
 #services.desktopManager.plasma5.enable = true;
-services.displayManager.sddm.enable = true;
+services.displayManager.gdm.enable = true;
 
 
 # Configure keymap in X11
