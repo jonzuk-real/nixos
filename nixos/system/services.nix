77ceb0b7c168ff0f97c92f651d28c4d0fd8570d9
jonzuk <jonzukspam@gmail.com>
2025-01-05 14:35:50 -0500

    loser remover on

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -97,7 +97,7 @@ environment.gnome.excludePackages = with pkgs.gnome; [
   ];
 
 #KDE Loser Remover
-#environment.plasma6.excludePackages = with pkgs.kdePackages; [
+environment.plasma6.excludePackages = with pkgs.kdePackages; [
   plasma-browser-integration
   konsole
   oxygen
