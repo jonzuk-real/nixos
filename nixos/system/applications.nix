73477ea30b9034df723f22c7bc7c5035f2c3a043
jonzuk <jonzukspam@gmail.com>
2025-01-16 14:28:04 -0500

    hyprshot

--- nixos/system/applications.nix
+++ nixos/system/applications.nix
@@ -118,7 +118,7 @@ environment.systemPackages = with pkgs; [
 	xfce.thunar-archive-plugin
 	xfce.thunar
 	xfce.thunar-volman
-#	hyprshot # Screenshot tool
+	hyprshot # Screenshot tool
 #	pkgs.grim # screenshot tool
 	pkgs.slurp
 	pkgs.wl-clipboard
