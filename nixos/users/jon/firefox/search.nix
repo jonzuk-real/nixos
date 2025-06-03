628243af7504d766c5189da13424a9396554fbfc
jonzuk <jonzukspam@gmail.com>
2025-06-03 17:53:05 -0400

    startpage

--- nixos/users/jon/firefox/search.nix
+++ nixos/users/jon/firefox/search.nix
@@ -1,7 +1,7 @@
  {
  programs.firefox.profiles.default.search = {
             force = true;
-            default = "DuckDuckGo";
+            default = "StartPage";
             order = [ "DuckDuckGo" "Searx" "Google" ];
             engines = {
 	      "Perplexity AI" = {
