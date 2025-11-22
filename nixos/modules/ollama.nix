1dedf72455985a8e6daa2ae344b58638beac4402
jonzuk <jonzukspam@gmail.com>
2025-11-22 04:17:43 -0500

    webui

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -10,6 +10,6 @@ services.ollama = {
   # Optional: preload models, see https://ollama.com/library
   loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
 };
-
+services.open-webui.enable = true;
 
 }
