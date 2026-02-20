adaddacda0be6624634b651c6d9d1d41bd0bdcb9
jonzuk <jonzukspam@gmail.com>
2026-02-20 00:32:59 -0500

    fuck you

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -21,9 +21,9 @@
    	virtualisation.virtualbox.host.enableExtensionPack = true;
 	services.spice-vdagentd.enable = true;	
 	services.mullvad-vpn.enable = true;
-	services.openssh.permitRootLogin = true;
-	services.openssh.enable = true;
-	services.openssh.settings.X11Forwarding = true;
+#	services.openssh.permitRootLogin = true;
+#	services.openssh.enable = true;
+#	services.openssh.settings.X11Forwarding = true;
 #	services.gnome.gnome-keyring.enable = true;
 	programs.xfconf.enable = true;
 	services.gvfs.enable = true; # Mount, trash, and other functionalities
