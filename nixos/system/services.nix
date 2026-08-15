33f08dd45ea6b69b83badffdb1b521f14a2f497a
jonzuk-real <jonzuk@protonmail.com>
2026-08-14 22:14:32 -0400

    turned off guest shit

--- nixos/system/services.nix
+++ nixos/system/services.nix
@@ -15,7 +15,7 @@
 	networking. enableIPv6 = false;
 	virtualisation.virtualbox.host.enable = true;
    	users.extraGroups.vboxusers.members = [ "jon" ];
-	virtualisation.virtualbox.guest.enable = true;
+#	virtualisation.virtualbox.guest.enable = true;
 #  	virtualisation.virtualbox.guest.dragAndDrop = true;
 #	virtualisation.virtualbox.host.enable = true;
 #   	virtualisation.virtualbox.host.enableExtensionPack = true;
